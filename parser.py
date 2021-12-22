de5ecafee12cd52993b93cc5b7684e4593cd6dba
kudryavtsev69 <kudryavtsev69@gmail.com>
2021-12-22 14:04:00 +0300

    Начало положенно Написаны функции получения запроса, получения контента и парсинга сайта новостей

---
+++ parser.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://ria.ru/world/"  # Адресс сайта новостей
+HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 '
+                         'YaBrowser/21.9.1.600 (beta) Yowser/2.5 Safari/537.36',
+           'accept': '*/*'}
+
+def get_html(url, params=None): # Запрос
+    r = requests.get(url, headers=HEADERS, params=params)
+    return  r
+
+def get_content(html): # Получение контента
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('a', class_='list-item__title color-font-hover-only')
+
+def parse():  #  Парсинг сайта новостей
+    html = get_html(URL)
+    if html.status_code == 200:
+        get_content(html.text)
+    else:
+        print('Error')
+
+parse()
